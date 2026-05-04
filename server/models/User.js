fd41a65c1a4223e585602a5464e25f89bc3651ac
lalith301 <lkmm334@gmail.com>
2026-05-04 17:07:39 +0530

    Remove phone field from User model

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     firebaseUid: { type: String, unique: true, sparse: true },
     email: { type: String, unique: true, sparse: true },
-    phone: { type: String, unique: true, sparse: true }, // sparse allows multiple nulls
     fullName: { type: String, required: true },
     password: { type: String, default: "" },
     profilePic: { type: String, default: "" },
@@ -11,5 +10,4 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
-
 export default User;
